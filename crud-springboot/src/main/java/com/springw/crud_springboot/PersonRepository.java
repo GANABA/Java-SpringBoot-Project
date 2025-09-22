12ce761c9ba48c6ac99bfc37778e7a35a34a50f4
GANABA Melchisedech <ganabamelchisedech@gmail.com>
2025-09-22 16:31:42 -0700

    CRUD avec SpringBoot

---
+++ crud-springboot/src/main/java/com/springw/crud_springboot/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.springw.crud_springboot;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
